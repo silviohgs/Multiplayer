a1c6dd4a13860c3103d39735eb5a1532d040eff6
silvioHGS <silviohgs@gmail.com>
2021-06-17 11:47:06 -0300

    primeiro commit
    primeiro comit

---
+++ Assets/Pixel Adventure 1/Assets/Script/BotoesMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class BotoesMenu : MonoBehaviour
+{
+
+    public string nomeCena;
+   
+    public void carregarCena()
+    {
+        
+                SceneManager.LoadScene(nomeCena);
+            
+    }
+
+    public void sair()
+    {
+        Application.Quit();
+    } 
+}
---
+++ Assets/Pixel Adventure 1/Assets/Script/Jogador.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+
+    private Rigidbody2D rig;
+    
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+    void Move()
+    {
+        Speed = 10;
+        Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movimento * Time.deltaTime * Speed;   
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+}
